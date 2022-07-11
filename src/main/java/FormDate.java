beecc5c79999f5bdccc10110a6e5e04023e90720
Olga Kapustina <hellog@rambler.ru>
2022-07-11 22:28:53 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+public class FormDate {
+    public static void main(String[] args) {
+        int FormDateDay = 13;
+        int FormDateMonth = 6;
+        int FormDateYear = 1999;
+
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public int FormDateDay;
+    public int FormDateMonth;
+    public int FormDateYear;
+
+
+}
